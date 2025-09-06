6ba96a6e1ac1ab32333c9ae5679da298a835d2cf
Rus1an13 <ruslan-1978@bk.ru>
2025-09-06 20:25:28 +0500

    Main again

--- website_root/website/settings.py
+++ website_root/website/settings.py
@@ -118,10 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-#     #  "/website_root/blog/static/",
-# ]
+STATICFILES_DIRS = [
+     BASE_DIR / "static",
+   #  "/website_root/blog/static/",
+ ]
 # MEDIA_URL = '/img/'
 # MEDIA_ROOT = BASE_DIR / "/img/"
 
